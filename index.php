30c2520028c693ba17122c46b94f551bbf125250
fahim100 <fahimAhmmed100@gmail.com>
2018-10-04 09:44:57 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,206 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>PSD to HTML</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="">
+
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+<div class="container full-page">
+	<div class="row">
+		<div class="header">
+			<div class="col-md-4 logo">
+				<p class="as">ASTOUNDING</p>
+				<p class="fr">Free PSD Website Template</p>
+			</div>
+			<div class="col-md-8 navbar">
+				<ul>
+					<li><a href="">HOME</a></li>
+					<li><a href="">STYLE DEMO</a></li>
+					<li><a href="">FULL WIDTH</a></li>
+					<li><a href="">DROPDOWN</a></li>
+					<li><a href="">PORTFOLIO</a></li>
+					<li><a href="">GALLERY</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="header-image">
+			<img src="image/header-image.jpeg" alt="Header image">
+			<p>A Massive <br> Title That Uses <br> Loads Of Room.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="service-part">
+			<h2 class="se"><b>OUR SERVICES</b></h2>
+			<p class="se-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, <br>sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. </p>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box">
+					<span><img class="service1" src="image/service1.png" alt="service1"></span>
+					<h4 class="title">SERVICE TITLE</h4>
+					<p class="description">Vestassapede et donec ut est libe ros sus et eget sed eget 
+					quisq ueta habitur augue</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box">
+					<span><img class="service1" src="image/service2.png" alt="service1"></span>
+					<h4 class="title">SERVICE TITLE</h4>
+					<p class="description">Vestassapede et donec ut est libe ros sus et eget sed eget 
+					quisq ueta habitur augue</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box">
+					<span><img class="service1" src="image/service3.png" alt="service1"></span>
+					<h4 class="title">SERVICE TITLE</h4>
+					<p class="description">Vestassapede et donec ut est libe ros sus et eget sed eget 
+					quisq ueta habitur augue</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box">
+					<span><img class="service1" src="image/service4.png" alt="service1"></span>
+					<h4 class="title">SERVICE TITLE</h4>
+					<p class="description">Vestassapede et donec ut est libe ros sus et eget sed eget 
+					quisq ueta habitur augue</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="team-part">
+			<h2 class="me"><b>MEET OUR TEAM MEMBERS</b></h2>
+			<div class="col-md-3 col-sm-6">
+				<div class="team-box">
+					<span><img src="image/team-mate2.JPG" alt="team mate1"></span>
+					<h4 class="name">Member name</h4>
+					<p class="role">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua.</p>
+					<a class="right" href="">View Profile »</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="team-box">
+					<span><img src="image/team-mate2.JPG" alt="team mate1"></span>
+					<h4 class="name">Member name</h4>
+					<p class="role">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua.</p>
+					<a class="right" href="">View Profile »</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="team-box">
+					<span><img src="image/team-mate2.JPG" alt="team mate1"></span>
+					<h4 class="name">Member name</h4>
+					<p class="role">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua.</p>
+					<a class="right" href="">View Profile »</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="team-box">
+					<span><img src="image/team-mate2.JPG" alt="team mate1"></span>
+					<h4 class="name">Member name</h4>
+					<p class="role">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua.</p>
+					<a class="right" href="">View Profile »</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="contact-section">
+			<div class="col-md-3 col-sm-6">
+				<div class="blog-box">
+					<h3 class="blog-title">From The Blog</h3>
+					<h4 class="post_title">Post Title</h4>
+					<p class="admin"><span>Admin</span>, domainname.com</p>
+					<p class="friday">Friday, 6th April 2000</p>
+					<p class="Vestibulumaccumsan">Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit 
+					intesque sed. Facilispede estibulum nulla orna nisl velit elit ac aliquat non
+					tincidunt.Namjusto cras urna urnaretra lor urna neque sed quis orci nulla.</p>
+					<a href="">Read more »</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="blog-box">
+					<h3>Quick Links</h3>
+					<ul>
+						<li><a href="">» Lorem ipsum dolor sit</a></li>
+						<li><a href="">» Amet consectetur</a></li>
+						<li><a href="">» Praesent vel sem id</a></li>
+						<li><a href="">» Curabitur hendrerit est</a></li>
+						<li><a href="">» Aliquam eget erat nec sapien</a></li>
+						<li><a href="">» Cras id augue nunc</a></li>
+						<li><a href="">» Sed a nulla urna</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="blog-box">
+					<h3>Latest Tweets</h3>
+					<p class="tweets"><span>@namehere</span> Justoid nonummy laoreet phasellent penatoque in antesque pellus elis eget tincidunt. 
+					Nequatdui laorem justo a non tellus laoremut vitae doloreet 1 day ago</p>
+					<p class="tweets"><span>@namehere</span> Justoid nonummy laoreet phasellent penatoque in antesque pellus elis eget tincidunt. 
+					Nequatdui laorem justo a non tellus laoremut vitae doloreet 1 day ago</p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="blog-box">
+					<h3>Contact Us</h3>
+					<form>
+						<div class="form-group">
+    						<input type="text" class="form-control" placeholder="Full Name">
+  						</div>
+  						<div class="form-group">
+    						<input type="email" class="form-control" placeholder="Email Address">
+  						</div>
+  						<div class="form-group">
+    						<input type="text" class="form-control" placeholder="Subject">
+  						</div>
+  						<div class="form-group">
+    						<textarea class="form-control" rows="3"  placeholder="Message"></textarea>
+  						</div>
+  						<div class="form-group">
+    						<button class="btn btn-info">Submit</button>
+  						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- footer section -->
+	<div class="row">
+		<div class="footer">
+			<div class="col-md-6 col-sm-6">
+				<p>&copy; 2013 Domain Name - All Rights Reserved</p>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<p class="right">Template by OS Templates</p>
+			</div>
+		</div>
+	</div>
+	<br><br>
+</div>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.js"></script>
+  <script src="js/jquery-2.2.0.min.js"></script>
+</body>
+</html>
